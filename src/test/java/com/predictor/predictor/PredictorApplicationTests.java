4e81383a91d73dee77a0f616a7cd706db628e7c9
Rupesh <rupesh.dubey@chqbook.com>
2021-07-22 22:37:13 +0530

    hello

---
+++ src/test/java/com/predictor/predictor/PredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predictor.predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
